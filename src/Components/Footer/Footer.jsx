887faabf08e09d1f1edec8775eeb2cc802b13671
miracle jane <miracleokon2003@gmail.com>
2026-01-19 12:01:18 +0100

    made changes on footer, deploy to react

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 const Footer = () => {
   return (
     <div>
-        <div className='w-full  h-80 md:px-20  '>
-            <div className='bg-[#faf0df]  shadow-sm w-full h-60 px-10 md:px-50 flex justify-between  '>
-                <div >
+        <div className='w-full  h-80 md:px-5   '>
+            <div className='bg-[#faf0df]  shadow-sm w-full h-60 px-5 md:px-20 lg:px-50  flex justify-between  '>
+                <div>
                     <p className='font-medium text-[1.5rem] mt-5  '>Quick Link</p>
                      <p className='mt-2'>Home</p>
                      <p className='mt-2'>Projects</p>
@@ -13,7 +13,7 @@ const Footer = () => {
                     <p className='mt-2'>Contact</p>
                 </div>
 
-                <div>
+                <div className=''>
                     <p className='font-medium text-[1.5rem] mt-5'>Services</p>
                     <p className='mt-2'>Web development</p>
                     <p className='mt-2'>Website maintenance</p>
@@ -27,7 +27,7 @@ const Footer = () => {
                     <p className='mt-2'>Abuja, Nigeria</p>
                 </div> */}
             </div>
-            <div className='bg-[#faf0df] border-t  h-20 w-full flex items-center justify-center '>
+            <div className='bg-[#faf0df] border-t border-gray-120  h-20 w-full flex items-center justify-center '>
                  <p>© 2026 Jane. All Rights Reserved.</p>
             </div>
         </div>
